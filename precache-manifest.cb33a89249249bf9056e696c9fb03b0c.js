ac959caaf1e134541150cf657c776b239810896a
Dmitry Boyarchik <dmitry.boyarchik@gmail.com>
2020-04-17 16:19:34 +0200

    initial commit

---
+++ precache-manifest.cb33a89249249bf9056e696c9fb03b0c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf90b855f7184e485917b925b14c7074",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dbc056826f51877aaef0",
+    "url": "/static/js/2.6218f11a.chunk.js"
+  },
+  {
+    "revision": "a459b3f8d602fdf864677274a5926294",
+    "url": "/static/js/2.6218f11a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cb457d7dd87def24bfd",
+    "url": "/static/js/3.f8f31c12.chunk.js"
+  },
+  {
+    "revision": "a62e072d6648eb2442b6",
+    "url": "/static/js/4.eabbe697.chunk.js"
+  },
+  {
+    "revision": "82b8ab2e136d6db2589b",
+    "url": "/static/js/main.6fe990cc.chunk.js"
+  },
+  {
+    "revision": "3855577e18ea0ecb28b7",
+    "url": "/static/js/runtime-main.16191e95.js"
+  },
+  {
+    "revision": "5bed1b9c4a8907f94d1e8438dee71aaf",
+    "url": "/static/media/completed.5bed1b9c.svg"
+  },
+  {
+    "revision": "656bc7d9adf204fe63f02d2d7ad23252",
+    "url": "/static/media/logo.656bc7d9.svg"
+  }
+]);
